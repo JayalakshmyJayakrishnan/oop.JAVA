d916d6f687b894b827d6dc11dd44f12293ff32d3
Jayalakshmy Jayakrishnan <lakshmystudy21@gmail.com>
2024-09-26 22:18:40 +0530

    ArrMod.java

---
+++ ArrMod.java
@@ -0,0 +1,8 @@
+class ArrMod{
+  public static void main(String[] args){
+    int[] arr ={20, 30, 40, 50};
+    arr[0] = 10;
+    System.out.println("The modified array is: " + java.util.toString(arr));
+  }
+}
+
