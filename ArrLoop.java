7e3f9ff994e3230792f155885598fdfbb1b3f995
Jayalakshmy Jayakrishnan <lakshmystudy21@gmail.com>
2024-09-26 22:37:18 +0530

    ArrLoop.java
    OUTPUT:
    The element at index 0 : 10
    The element at index 1 : 20
    The element at index 2 : 30
    The element at index 3 : 40
    The element at index 4 : 50

---
+++ ArrLoop.java
@@ -0,0 +1,8 @@
+class ArrLoop{
+  public static void main(String[] args){    
+    int[] arr = {10, 20, 30, 40, 50};
+    for(int i=0;i<arr.length;i++){
+      System.out.println("The element at index " + i + " : " + arr[i]);
+    }
+  }
+}
