a93175d347ba2c333d1a4b7434dbe85e0ff7293e
Jayalakshmy Jayakrishnan <lakshmystudy21@gmail.com>
2024-09-26 21:11:10 +0530

    Array.java

--- Array.java
+++ Array.java
@@ -1,4 +1,4 @@
-public class Main{
+public class Array{
   public static void main(String[] args){
     int[] arr = {10, 20, 30, 40};
     System.out.println("Array created with elements: " + java.util.Arrays.toString(arr));
