2053c6582f931580ed504b29ab2af3ff5d5d81ec
Jayalakshmy Jayakrishnan <lakshmystudy21@gmail.com>
2024-09-26 22:08:04 +0530

    ArrAccess.java

--- ArrAccess.java
+++ ArrAccess.java
@@ -1,8 +1,8 @@
 class ArrAccess{
   public static void main(String[] args){
     int[] arr = {10,20,30,40,50};
-    System.out.println("The first element is: " + arr[1]);
-    System.out.println("The second element is: " + arr[2]);
+    System.out.println("The first element is: " + arr[0]);
+    System.out.println("The second element is: " + arr[1]);
       }
 }
   
