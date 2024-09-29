2f493311c22337ab43194f8f9b36430b1cfa06a2
Jayalakshmy Jayakrishnan <lakshmystudy21@gmail.com>
2024-09-29 20:48:06 +0530

    SecSmall.java

---
+++ SecSmall.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+class SecSmall {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements in the array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int smallest = Integer.MAX_VALUE;
+        int secondSmallest = Integer.MAX_VALUE;
+
+        for (int num : arr) {
+            if (num < smallest) {
+                secondSmallest = smallest;
+                smallest = num;
+            } else if (num < second && num != smallest) {
+                second = num;
+            }
+        }
+
+        System.out.println("The second smallest element is: " + second);
+    }
+}
