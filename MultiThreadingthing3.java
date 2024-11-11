254f9aae34c34e8dcec9eac15b0b415fdf089814
Jayalakshmy Jayakrishnan <lakshmystudy21@gmail.com>
2024-11-11 19:48:32 +0530

    MultiThreadingthing3.java
    By calling thread.join(), you ensure that each thread completes its execution before the next thread starts. This synchronizes the output, and you'll see the complete output of one thread before moving on to the next.

---
+++ MultiThreadingthing3.java
@@ -0,0 +1,68 @@
+import java.util.*;
+
+class MultiThreadingthing implements Runnable {
+
+    private int threadNumber;
+
+    public MultiThreadingthing(int threadNumber) {
+        this.threadNumber = threadNumber;
+    }
+
+    @Override
+    public void run() {
+        for(int i = 1; i<=5; i++) {
+            System.out.println(i + " FROM THREAD " + threadNumber);
+
+            try{
+                Thread.sleep(1000);
+            } catch(InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+public class MultiThreadingthing3 {
+    public static void main(String[] args) {
+        for(int i = 0; i<5; i++) {
+            MultiThreadingthing mythread1 = new MultiThreadingthing(i);
+            Thread thread = new Thread(mythread1);
+            thread.start();
+                try {
+                      thread.join();
+                  } catch(InterruptedException e) {
+                              e.printStackTrace();
+                       }
+        }
+    }
+}
+
+/* 
+OUTPUT:
+1 FROM THREAD 0
+2 FROM THREAD 0
+3 FROM THREAD 0
+4 FROM THREAD 0
+5 FROM THREAD 0
+1 FROM THREAD 1
+2 FROM THREAD 1
+3 FROM THREAD 1
+4 FROM THREAD 1
+5 FROM THREAD 1
+1 FROM THREAD 2
+2 FROM THREAD 2
+3 FROM THREAD 2
+4 FROM THREAD 2
+5 FROM THREAD 2
+1 FROM THREAD 3
+2 FROM THREAD 3
+3 FROM THREAD 3
+4 FROM THREAD 3
+5 FROM THREAD 3
+1 FROM THREAD 4
+2 FROM THREAD 4
+3 FROM THREAD 4
+4 FROM THREAD 4
+5 FROM THREAD 4
+
+*/
